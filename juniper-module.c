87388a2c4de0b531dd0544bf0c1320fabaaf4e45
Joseph Novakovich <josephnovakovich@gmail.com>
2020-09-06 21:24:26 -0400

    is this how?

--- juniper-module.c
+++ juniper-module.c
@@ -12,6 +12,8 @@ int plugin_is_GPL_compatible;
 
 #include "juniper-module.h"
 
+#define R return
+
 typedef V* (*JIT)(); // j init type
 typedef V* (*JDT)(); // j do   type
 typedef V* (*JFT)(); // j free type
@@ -19,9 +21,18 @@ typedef emacs_value EV;
 typedef emacs_env EE;
 typedef struct emacs_runtime ERT;
 
+static V *jh; // dll handle
+static JDT jdo;
+static JFT jfree;
+static JIT jinit;
+
 // need to get a hold of the j engine and execute the sentence
 static EV jedo (EE *e,ptrdiff_t n, EV *args, V *ptr) {
   J j = e->get_user_ptr(e,args[0]);
+}
+
+static EV jeini (EE *e,ptrdiff_t n, EV *args, V *ptr) {
+  R e->make_user_ptr(e,(V*)jfree,jinit());
 }
 
 // dll functions:
@@ -29,10 +40,10 @@ static EV jedo (EE *e,ptrdiff_t n, EV *args, V *ptr) {
 int emacs_module_init (ERT *rt) {
   EE *e = rt->get_environment(rt);
   
-  V *jh = dlopen(LIBJ,RTLD_LAZY);
-  J j=((JIT)dlsym(jh,"JInit"))();
-  JDT jdo=((JDT)dlsym(jh,"JDo"));
-  JFT jfree=((JFT)dlsym(jh,"JFree"));
+  jh = dlopen(LIBJ,RTLD_LAZY);
+  jinit = (JIT)dlsym(jh,"JInit");
+  jdo=((JDT)dlsym(jh,"JDo"));
+  jfree=((JFT)dlsym(jh,"JFree"));
 
   EV provide = e->intern(e, "provide");
   EV fset = e->intern(e,"fset");
@@ -40,12 +51,12 @@ int emacs_module_init (ERT *rt) {
   EV args[2];
 
   args[0]=e->intern(e,"j-engine");
-  args[1]=e->make_user_ptr(e,(V*)jfree,j);
-  e->funcall(e,set,2,args);
+  args[1]=e->make_function(e,0,0,jeini,"Create a J Engine",NULL);
+  e->funcall(e,fset,2,args);
 
   args[0] = e->intern(e, "juniper-module");
   e->funcall(e,provide,1,args);
-  return 0;
+  R 0;
 }
 
 // some resources in addition to emacs docs:
