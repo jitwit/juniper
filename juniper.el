7be689f6b4004220e74ee2454598b3738cfd07d5
Joseph Novakovich <josephnovakovich@gmail.com>
2020-09-07 16:42:15 -0400

    J!

--- juniper.el
+++ juniper.el
@@ -6,14 +6,33 @@
   "/home/jrn/.guix-profile/bin/profile.ijs"
   "your J initialization script")
 
+(defcustom juniper-buffer
+  "*juniper*"
+  "juniper buffer")
+
 (defun new-j ()
-  "create a J engine, including initialization"
+  "create and initialize a J engine"
   (let ((J (j-engine)))
-    (j-do J "BINPATH_z_ =: 1!:43''")
     (j-do J "ARGV_z_ =: 'emacs'")
     (j-do J (concat "0!:0 < '" juniper-profile-ijs "'"))
-    (delete-file "juniper.txt") ; NB. need to decide how to do buffers/engines
-    (j-smx J "juniper.txt")
+    (j-do J "BINPATH_z_ =: 1!:43''")
     J))
+
+(defvar J
+  (new-j)
+  "quick and dirty world-wide J")
+
+(defun j-eval (J sentence)
+  (let ((j-out (make-temp-file "juniper")))
+    (j-smx J j-out)
+    (j-do J sentence)
+    (insert-file-contents j-out)))
+
+(defun j-mini (sentence)
+  "execute J sentence from mini buffer"
+  (interactive "sJ: ")
+  (with-temp-buffer
+    (j-eval J sentence)
+    (display-message-or-buffer (buffer-string))))
 
 (provide 'juniper)
--- test.el
+++ test.el
@@ -7,9 +7,11 @@
 (defvar ej (new-j))
 
 (defun fake-test ()
+  (j-smx ej "juniper.txt")
   (j-do ej "i. 10")
   (j-do ej "''[(a.{~65+(?~26){i.26) (1!:2) 4")
   (j-do ej "%. ? 3 3 $ 0")
+  (j-do ej "echo BINPATH")
   (j-do ej "BINPATH -: 1!:43 ''")
   (j-do ej "ARGV")
   (j-do ej "echo JVERSION"))
