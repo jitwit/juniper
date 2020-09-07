9b49e36ae8836c7591a7377dc0b13a67800e6f09
Joseph Novakovich <josephnovakovich@gmail.com>
2020-09-07 13:55:51 -0400

    more screwing around

--- test.el
+++ test.el
@@ -7,12 +7,17 @@
 (defvar ej (j-engine))
 
 (defun fake-test ()
-  (delete-file "blaha.txt")
+  ;; (delete-file "blaha.txt")
   (j-smx ej "blaha.txt")
   (j-do ej "i. 10")
-  ;; (j-do ej "(a.{~65+(?~26){i.26) (1!:2) 4")
+  (j-do ej "%. ?. 3 3 $ 0")
+  (j-do ej "''[(a.{~65+(?~26){i.26) (1!:2) 4")
+  (j-do ej "1!:43 ''")
+  (j-do ej "1!:44 '/home/jrn'")
+  (j-do ej "1!:43 ''")
+  ;; (j-do ej "2!:55 ''") <- exits emacs! is this wrong?
   )
+
 ;; < 'blaha.txt'"))
 
 (fake-test)
-
